8e07af0973104fb7a22700739c5431bcd22a0c5e
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-12-28 20:53:31 +0530

    stable, ValidateLength is having problems

--- src/Checkbox.js
+++ src/Checkbox.js
@@ -1,4 +1,4 @@
-//import React from "react";
+//import React from "react"; 
 import * as React from 'react';
 import TakeInputBackup from './TakeInputBackup.js';
 
--- src/TakeInputBackup.js
+++ src/TakeInputBackup.js
@@ -1,16 +1,23 @@
 import React from "react";
 
 	let suggestion="";
-	var number=['1','2','3','4','5','6','7','8','9','0'];
-	var special_char=['!','@','#','$','%','&','*'];
-	var letter=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-	var alpha=['A','B', 'C','D','E','F','G','H','I','J','K','L','M','N', 'O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-function  Driver( max, min,numObj, specialObj, lettersObj, alphabetsObj) {
+	function  Driver( max, min, numObj, specialObj, lettersObj, alphabetsObj) {
 	const [password, setPassword]=React.useState("");
 	const [pass_length, setLength]=React.useState(0);
-	setLength(ValidateLength(max, min));
-	console.log("test for length> "+pass_length);
+	const [data, setData]=React.useState([]);
+	setLength(9);
+	
+
+	// console.log("test for length> "+pass_length);
+
+	const [numUser, setNum]=React.useState(Boolean(numObj));
+	const [specialUser, setSpecial]=React.useState(Boolean(specialObj));
+	const [letterUser, setLetters]=React.useState(Boolean(lettersObj));
 	suggestion=password;
+	const [alphaUser, setAlpha]=React.useState(Boolean(alphabetsObj));
+
+	fillData(numUser, specialUser, letterUser, alphaUser, data);
+console.log("dsdn");
 }
 
 function copy(tocopy){
@@ -21,7 +28,15 @@ function random( max,min){//this function is working...
 	min=Math.ceil(min);max=Math.floor(max);
 	return Math.floor(Math.random()*(max-min)+min);
 	}
+function fillData(N, S, L, A, data){
+	// var number=['1','2','3','4','5','6','7','8','9','0'];
+	// var special_char=['!','@','#','$','%','&','*'];
+	// var letter=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+	// var alpha=['A','B', 'C','D','E','F','G','H','I','J','K','L','M','N', 'O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+console.log(typeof(N));
+// if(N==true)
 
+}
 function makePswd(data, len){
 	//console.log("from makePsw");
 	//console.log(data+" >> "+len);
@@ -47,7 +62,7 @@ function ValidateLength(max, min){
 		setLen(random(max,min));
 	}
 return len;
-	// console.log(typeof(max));
+	// console.log("test");
 }
 
 function TakeInputBackup(props){
@@ -94,9 +109,9 @@ return(
 	<h2> Including Special Characters? {props.spc_char.toString()}</h2>
 	<h2> Including Letters? {props.letters.toString()}</h2>
 	<h2> Including Alphabets? {props.alphabets.toString()}</h2>
-{/*
+
 <button style={hide} type="button" onChange={Driver(props.max, props.min, props.num, props.spc_char, props.letters, props.alphabets)}></button>
-*/}
+
 
 	<h2 style={suggest}>Suggestion: **{suggestion.toString()}** </h2>
 	<button style={hide} type="button" onChange={ copy() }></button>
