035241de223822af893416fae27732cc25759aa0
saptarshi chattopadhyay <saptarshichattopadhyay543@gmail.com>
2022-12-28 19:49:33 +0530

    Revert "removed temporary test code from PROD"
    This reverts commit c684e5d8ef37b1c7c922d45d67f825be9d38f0d8.

--- src/TakeInput.js
+++ src/TakeInput.js
@@ -89,7 +89,7 @@ function ValidateFinalArray(max, min,numObj, specialObj, lettersObj, alphabetsOb
 	// 		forCheck=makePswd(final_data, final_length);
 	// 	}
 	// }
-	//var t=checkPswd(forCheck, numbersUser, specialUser, lettersUser, alphabetsUser);
+	var t=checkPswd(forCheck, numbersUser, specialUser, lettersUser, alphabetsUser);
 //console.log("check> "+t);
 	copy();
 }
